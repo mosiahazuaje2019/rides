4b9b80b17501ccaff17002599782da0e733bc05d
Elieser Reyes <platalunasol@gmail.com>
2023-08-11 01:47:59 -0400

    removida la paginacion por sugerencia de alex

--- resources/js/Pages/Bookings/BookingAdmin.vue
+++ resources/js/Pages/Bookings/BookingAdmin.vue
@@ -188,9 +188,6 @@ const bookingFilter = () => {
 
         <DataTable
             v-model:filters="filters"
-            paginator
-            :rows="5"
-            :rowsPerPageOptions="[5, 10, 20, 50]"
             :value="rides"
             editMode="cell"
             @cell-edit-complete="onCellEditComplete"
